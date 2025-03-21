8b6e9e9a7552a8cb30f9d5616d22055ba1cdf81e
Fantamomo <fantamomo@gmx.at>
2025-03-21 13:40:32 +0100

    add abstract class LumenPlugin with lifecycle methods and logger support

--- src/main/kotlin/at/leisner/lumen/api/plugin/LumenPlugin.kt
+++ src/main/kotlin/at/leisner/lumen/api/plugin/LumenPlugin.kt
@@ -1,4 +1,17 @@
 package at.leisner.lumen.api.plugin
 
-class LumenPlugin {
+import at.leisner.lumen.api.Lumen
+import java.util.logging.Logger
+
+abstract class LumenPlugin {
+    lateinit var lumen: Lumen
+        internal set
+    lateinit var logger: Logger
+        internal set
+
+    open fun onLoad() {}
+
+    open fun onEnable() {}
+
+    open fun onDisable() {}
 }
