0b7f6bef3bf30a29a89b85ce4b18f614699d2f11
Fantamomo <fantamomo@gmx.at>
2025-03-21 13:42:55 +0100

    add utility functions for QueryResult manipulation and mapping

--- src/main/kotlin/at/leisner/lumen/api/result/util.kt
+++ src/main/kotlin/at/leisner/lumen/api/result/util.kt
@@ -1,2 +1,19 @@
 package at.leisner.lumen.api.result
 
+fun queryResultOf(
+    title: String,
+    subtitle: String,
+    priority: Int = QueryResult.DEFAULT_PRIORITY,
+    override: Boolean = false,
+) = DirectQueryResult(title, subtitle, priority, override)
+
+fun QueryResult.toMapped(actions: Set<MappedQueryResult.Action> = setOf(), onEnter: () -> Unit = {}): MappedQueryResult =
+    MappedQueryResult(this, actions, onEnter)
+
+fun QueryResult.toList(): List<QueryResult> = listOf(this)
+
+fun mappedQueryResultBuilder(action: MappedQueryResultBuilder.() -> Unit): MappedQueryResult {
+    val builder = MappedQueryResultBuilder()
+    builder.action()
+    return builder.build()
+}
