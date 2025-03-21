e0ca5e904bbf186a9c615f13e35c1b300fc9a792
Fantamomo <fantamomo@gmx.at>
2025-03-21 13:35:40 +0100

    add Maven publishing configuration and update dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,23 +4,29 @@ plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("maven-publish")
 }
 
 group = "at.leisner.lumen"
 version = "1.0-SNAPSHOT"
+val versionString = version as String
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs)
+    api("at.leisner:Composing:1.0-SNAPSHOT")
+    api("at.leisner:K-Framework:1.0-SNAPSHOT")
+    api(compose.desktop.currentOs)
+
+    api("org.jetbrains.kotlin:kotlin-scripting-common")
+    api("org.jetbrains.kotlin:kotlin-scripting-jvm")
+    api("org.jetbrains.kotlin:kotlin-scripting-dependencies")
+    api("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven")
 }
 
 compose.desktop {
@@ -32,5 +38,21 @@ compose.desktop {
             packageName = "Lumen-API"
             packageVersion = "1.0.0"
         }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            artifactId = "Lumen-API"
+            version = versionString
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        mavenLocal()
     }
 }
