1ddfcdd0cab9806855e5316baf62b3dfea4dfb27
Fantamomo <fantamomo@gmx.at>
2025-03-21 13:41:15 +0100

    add PluginInfo annotation with additional metadata fields

--- src/main/kotlin/at/leisner/lumen/api/plugin/PluginInfo.kt
+++ src/main/kotlin/at/leisner/lumen/api/plugin/PluginInfo.kt
@@ -1,3 +1,12 @@
 package at.leisner.lumen.api.plugin
 
-annotation class PluginInfo()
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class PluginInfo(
+    val id: String,
+    val name: String,
+    val author: Array<String> = [],
+    val description: String = "",
+    val version: String = "1.0-SNAPSHOT",
+    val dependencies: Array<Dependency> = []
+)
