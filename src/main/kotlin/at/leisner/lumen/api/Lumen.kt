77f274a8cae776fe0eb0f078985e0e8556086758
Fantamomo <fantamomo@gmx.at>
2025-03-21 13:40:22 +0100

    add methods for managing result providers and plugin configurations in Lumen interface

--- src/main/kotlin/at/leisner/lumen/api/Lumen.kt
+++ src/main/kotlin/at/leisner/lumen/api/Lumen.kt
@@ -1,4 +1,17 @@
 package at.leisner.lumen.api
 
+import at.leisner.lumen.api.provider.ProviderConfig
+import at.leisner.lumen.api.plugin.LumenPlugin
+import at.leisner.lumen.api.provider.ResultProvider
+
 interface Lumen {
+    fun registerResultProvider(plugin: LumenPlugin, provider: ResultProvider)
+
+    fun unregisterResultProvider(plugin: LumenPlugin, provider: ResultProvider)
+
+    fun unregisterAllResultProvider(plugin: LumenPlugin)
+
+    fun getPlugin(id: String): LumenPlugin?
+
+    fun registerProviderConfig(plugin: LumenPlugin, builder: ProviderConfig.ProviderConfigBuilder.() -> Unit): ProviderConfig
 }
